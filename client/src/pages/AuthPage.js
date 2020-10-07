9af889b972fb373ed926b8ec2ba6085b3a233abf
Yuritsuki <samatkamiev@gmail.com>
2020-10-07 17:31:12 +0600

    start working on requests clientside

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -1,20 +1,70 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {useHttp} from "../hooks/http.hook";
 
 export const AuthPage = () => {
-  return(
+    const {loading, request} = useHttp();
+    const [form, setForm] = useState({
+        email:'', password: ''
+    });
 
+    const changeHandler = event => {
+        setForm({ ...form, [event.target.name]: event.target.value });
+    };
+
+    const registerHandler = async() => {
+        try {
+            const data = await request('/api/auth/register', 'POST', {...form});
+            console.log('Data', data);
+        } catch(e) {}
+    };
+
+    return (
       <div className="row">
           <div className="col s6 offset-s3">
               <h1>Shorten your link</h1>
-              <div className="card blue-grey darken-1">
+              <div className="card blue darken-1">
                   <div className="card-content white-text">
-                      <span className="card-title">Card Title</span>
-                      <p>I am a very simple card. I am good at containing small bits of information.
-                          I am convenient because I require little markup to use effectively.</p>
+                      <span className="card-title">Authorization</span>
+                      <div>
+                          <div className="input-field">
+                              <input
+                                  placeholder="Type email"
+                                  id="email"
+                                  type="text"
+                                  name="email"
+                                  className="yellow-input"
+                                  onChange={changeHandler}
+                              />
+                                  <label htmlFor="email">Email</label>
+                          </div>
+                          <div className="input-field">
+                              <input
+                                  placeholder="Type password"
+                                  id="password"
+                                  type="password"
+                                  name="password"
+                                  className="yellow-input"
+                                  onChange={changeHandler}
+                              />
+                              <label htmlFor="email">Email</label>
+                          </div>
+                      </div>
                   </div>
                   <div className="card-action">
-                      <a href="#">This is a link</a>
-                      <a href="#">This is a link</a>
+                      <button
+                          className="btn yellow darken-4"
+                          style={{marginRight: 10}}
+                          disabled={loading}
+                      >
+                          Sign in
+                      </button>
+                      <button
+                          className="btn grey lighten-1 black-text"
+                          onClick={registerHandler}
+                          disabled={loading}
+                      >
+                          Sign up
+                      </button>
                   </div>
               </div>
           </div>
