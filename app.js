f08dd8345132278e2588a019ef46f2790db274e9
unknown <metavice@gmail.com>
2020-10-07 12:59:28 +0600

    paused

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 const express = require('express');
 const config = require('config');
+const mongoose = require('mongoose');
 
 const app = express();
 
-const PORT = config.get('port');
+app.use('/api/auth',)
 
-app.listen(5000, () => console.log('App has been started...'));
+const PORT = config.get('port') || 5000;
+
+async function start() {
+    try {
+        await mongoose.connect(config.get('mongoUri'), {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        });
+        app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`));
+
+    } catch (e) {
+        console.log('Server Error', e.message);
+        process.exit(1);
+    }
+}
+
+start();
