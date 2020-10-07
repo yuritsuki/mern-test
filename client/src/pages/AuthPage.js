ae48ed5204d1226af83fa47f03fd03c678c7ebb2
Yuritsuki <samatkamiev@gmail.com>
2020-10-07 16:54:44 +0600

    gitignore and change git user

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 export const AuthPage = () => {
   return(
+
       <div className="row">
           <div className="col s6 offset-s3">
               <h1>Shorten your link</h1>
